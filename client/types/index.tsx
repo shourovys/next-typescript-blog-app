3e16677aa4f6f45a36645befd798ed09c905037d
Shourov Saha <shourovys@gmail.com>
2022-12-27 08:26:27 +0600

    author and articles type added

--- client/types/index.tsx
+++ client/types/index.tsx
@@ -22,3 +22,54 @@ export interface ICategoryAttribute {
   title: string;
   slug: string;
 }
+
+// author
+export interface IAuthor {
+  id: number;
+  attributes: IAuthorAttribute;
+}
+export interface IAuthorAttribute {
+  username: string;
+  email: string;
+  provider: string;
+  firstName: string; // net to change in content type
+  lastName: string;
+  formats: {
+    thumbnail: {
+      url: string;
+    };
+  };
+}
+
+export interface IBlogImage {
+  data: {
+    attributes: {
+      url: string;
+      formats: {
+        small: {
+          url: string;
+        };
+      };
+    };
+  };
+}
+
+// Articles
+export interface IArticlesAttribute {
+  title: string;
+  slug: string;
+  shortDescription: string;
+  publishedAt: string;
+  body: string;
+  image: IBlogImage;
+  category: {
+    data: ICategory;
+  };
+  author: {
+    data: IAuthor;
+  };
+}
+export interface IArticles {
+  id: number;
+  attributes: IArticlesAttribute;
+}
